2e432e0e0f29f26a3ec1ad9e33832f972f72b999
Scott Chung <scottc52@gmail.com>
2012-06-13 12:59:40 -0700

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,33 +4,52 @@ describe "User pages" do
 
   subject { page }
   
-describe "index" do
+	describe "index" do
 
-    let(:user) { FactoryGirl.create(:user) }
+		let(:user) { FactoryGirl.create(:user) }
 
-    before(:all) { 30.times { FactoryGirl.create(:user) } }
-    after(:all)  { User.delete_all }
+		before(:all) { 30.times { FactoryGirl.create(:user) } }
+		after(:all)  { User.delete_all }
 
-    before(:each) do
-      sign_in user
-      visit users_path
-    end
+		before(:each) do
+		  sign_in user
+		  visit users_path
+		end
 
-    it { should have_selector('title', text: 'All users') }
-    it { should have_selector('h1',    text: 'All users') }
+		it { should have_selector('title', text: 'All users') }
+		it { should have_selector('h1',    text: 'All users') }
 
-    describe "pagination" do
+		describe "pagination" do
 
-      it { should have_selector('div.pagination') }
+		  it { should have_selector('div.pagination') }
 
-      it "should list each user" do
-        User.paginate(page: 1).each do |user|
-          page.should have_selector('li', text: user.name)
-        end
-      end
-    end
-  end
+		  it "should list each user" do
+			User.paginate(page: 1).each do |user|
+			  page.should have_selector('li', text: user.name)
+			end
+		  end
+		end
+		
+		describe "delete links" do
 
+		  it { should_not have_link('delete') }
+
+		  describe "as an admin user" do
+			let(:admin) { FactoryGirl.create(:admin) }
+			before do
+			  sign_in admin
+			  visit users_path
+			end
+
+			it { should have_link('delete', href: user_path(User.first)) }
+			it "should be able to delete another user" do
+			  expect { click_link('delete') }.to change(User, :count).by(-1)
+			end
+			it { should_not have_link('delete', href: user_path(admin)) }
+		  end
+		end
+	  end
+	
   
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
@@ -88,7 +107,7 @@ describe "index" do
     before do
        sign_in user
 	   visit edit_user_path(user)
-	emd
+	end
 
     describe "page" do
       it { should have_selector('h1',    text: "Update your profile") }
@@ -120,4 +139,5 @@ describe "index" do
       specify { user.reload.email.should == new_email }
     end
   end
+
 end
